2b7adbb6f70a3805136a3df8b571194d6ef6ad2c
Hiten Vaid <hitenvaid3837@gmail.com>
2025-11-25 01:16:14 +0530

    added test component

---
+++ App.jsx
@@ -0,0 +1,66 @@
+import { useState,useEffect } from 'react'
+import './App.css'
+import Home from "./component/Home"
+import Togglebtn from './component/togglebtn'
+import Dashboard from './component/Dashboard'
+import Input from './component/Input'
+import UseRef from "./component/UseRef";
+import ImageZoomViewer from './component/ImageZoomViewer'
+function App() {
+  const [count, setCount] = useState(0) //here it is destructing of useState which accepts a variable and setter func 
+
+    //useEffect takes 2 args {} func & [] dependency array
+    //mounting -> when DOM is created first time and appear on screen when creaation or component insetion
+    //updating ->Component re-renders when state/props change
+    //un-mounting -> delete the component
+
+  useEffect(()=>{
+    console.log("Component Mounted");
+  },[]); 
+
+  const info={
+    name: "hiten",
+    age:21,
+    skills: ["java","js","react","node"]
+  };
+  return (
+    <>
+      <div>
+        <div className="bg-linear-to-r from-indigo-400 to-green-400 p-2 rounded-xl">
+          This is my new heading
+        </div>
+
+        <div className="flex flex-col p-2 m-2 justify-center">
+          <div className="flex justify-center gap-2"> 
+            <h2 className="text-2xl font-bold p-2 m-2 bg-amber-500 rounded-lg">Current Count: {count}</h2>
+            <button onClick={() => setCount(count + 1)} 
+                className="bg-blue-500 text-white p-2 m-2 rounded-lg hover:bg-blue-300">
+              Increase Count
+            </button>
+            <button onClick={() => setCount(count - 1)} 
+                className="bg-blue-500 text-white p-2 m-2 rounded-lg hover:bg-blue-300">
+              Decrease Count
+            </button>
+            <button onClick={() => setCount(0)} 
+                className="bg-blue-500 text-white p-2 m-2 rounded-lg hover:bg-blue-300">
+              Reset Count
+            </button>
+          </div>
+        </div>
+        
+        <Home information={info}></Home>
+        
+        <Dashboard information={info}></Dashboard> 
+
+        <Togglebtn> </Togglebtn> 
+        <Input/>
+        <UseRef/>
+        <div className="p-2 m-2 ">
+          <ImageZoomViewer src="https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630"/>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
